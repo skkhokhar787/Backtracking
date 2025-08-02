a0166a25ae69c37e07713c89955b1975b71190af
Sameer Khokhar <skkhokhar787@gmail.com>
2025-08-02 13:09:29 +0500

    Add Sudoku solver implementation and project configuration

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,28 +1,76 @@
 public class Sudoku {
-    public static boolean helper (char [][] board, int row, int col) {
 
-        int nRow = 0;
-        int nCol = 0;
-
-        if (col == board.length-1) {
-            nRow = row + 1;
-        }
-        if (row == board.length-1) {
-            nCol = col + 1;
-        }
-        if (board[row][col] != '.') {
-            if (helper(board, row, col)) {
-                return true;
-            }
-        }else {
-
-            for (int i = 1; i <= 9; i++) {
-
+    public boolean solve(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) { // Empty cell
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0; // Backtrack
+                        }
+                    }
+                    return false; // No valid number found
+                }
             }
         }
+        return true; // Solved
+    }
+
+    private boolean isValid(int[][] board, int row, int col, int num) {
+        // Check row and column
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 subgrid
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
-    public static void main (String [] args) {
 
+    // Utility method to print the board
+    public void printBoard(int[][] board) {
+        for (int[] row : board) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Main method to test
+    public static void main(String[] args) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        Sudoku solver = new Sudoku();
+        if (solver.solve(board)) {
+            solver.printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
     }
 }
