93f08aaeb1be11b487288533ce3102b7f6f1ccb1
Sameer Khokhar <skkhokhar787@gmail.com>
2025-08-02 11:34:06 +0500

    Add Refactor permutation logic and add project configuration files

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,18 +1,23 @@
+import java.util.*;
+
 public class Permutation {
-    public static void printPerm (String str, int idx, String permutation) {
-        if (idx == 0) {
-            System.out.println(permutation);
+    static List<String> permutations = new ArrayList<>();
+
+    public static void printPerm(String str, String permutation) {
+        if (str.isEmpty()) {
+            permutations.add(permutation);
             return;
         }
-
         for (int i = 0; i < str.length(); i++) {
             char currentChar = str.charAt(i);
-            String  newStr = str.substring(0,i) + str.substring(i+1);
-            printPerm(newStr, idx+1, permutation + currentChar);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            printPerm(newStr, permutation + currentChar);
         }
-
     }
-    public static void main (String [] args) {
+
+    public static void main(String[] args) {
         String str = "ABC";
+        printPerm(str, "");
+        System.out.println("Last permutation: " + permutations.get(permutations.size() - 1));
     }
 }
